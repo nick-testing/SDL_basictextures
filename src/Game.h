d4cc5e61bc6912bb33fec8b3fb16409fdfcb682b
Nick <nick@example.com>
2024-08-15 00:35:27 +0300

    Added Game class skeleton

---
+++ src/Game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+typedef struct SDL_Window SDL_Window;
+typedef struct SDL_Surface SDL_Surface;
+typedef struct SDL_Renderer SDL_Renderer;
+typedef struct SDL_Texture SDL_Texture;
+
+class Game {
+public:
+    Game();
+    void Run();
+
+private:
+    static const int SCREEN_HEIGHT = 600;
+    static const int SCREEN_WIDTH = 800;
+
+    SDL_Window* window;
+    SDL_Surface* windowSurface;
+    SDL_Renderer* renderer;
+
+    static bool Init();
+    static bool LoadMedia();
+    SDL_Texture* LoadTexture(const char*);
+    static void EventHandler();
+    static void Close();
+};
+
+#endif
